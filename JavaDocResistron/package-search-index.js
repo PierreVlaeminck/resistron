e7809f183629f616e39a21d0c3ab3bacae1c3f13
PierreVlaeminck <121941634+PierreVlaeminck@users.noreply.github.com>
2023-02-03 13:01:02 +0100

    Add files via upload

---
+++ JavaDocResistron/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.resistron","l":"com.example.resistron"}];updateSearchResults();
---
+++ JavaDocResistron/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.resistron","l":"ResistronApplication"},{"p":"com.example.resistron","l":"ResistronController"}];updateSearchResults();
